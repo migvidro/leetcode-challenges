c8bf7c81ce3049fe1bc5c905dae74879b14641f1
Miguel Vidigal <migvidro@gmail.com>
2025-01-02 19:11:56 -0300

    adding max number of vowels challenge

---
+++ README.md
@@ -0,0 +1,35 @@
+# Leetcode Challenges 🚀
+
+Welcome to my repository of Leetcode challenges! This is where I document my solutions to coding problems from [Leetcode](https://leetcode.com/). The goal is to improve my problem-solving skills, explore different algorithms and data structures, and keep track of my progress.
+
+## Repository Structure 📂
+
+The repository is organized as follows:
+- **`/easy/`**: Solutions to Easy-level problems
+- **`/medium/`**: Solutions to Medium-level problems
+- **`/hard/`**: Solutions to Hard-level problems
+
+Each folder contains files named as `problem_name-problem_id.extension` (e.g., `two_sum-001.py`), along with comments explaining the approach and logic behind the solution.
+
+## Topics Covered 📚
+- Arrays
+- Strings
+- Linked Lists
+- Trees
+- Graphs
+- Dynamic Programming
+- Greedy Algorithms
+- Backtracking
+- And more!
+
+## How to Use
+1. Clone the repository
+2. Navigate to the desired difficulty folder to find solutions.
+3. Feel free to suggest improvements or alternative solutions via pull requests!
+
+## Contribution 🤝
+Contributions are welcome! If you'd like to contribute:
+
+1. Fork the repository.
+2. Add your solution in the appropriate folder.
+3. Submit a pull request with details about the problem and your approach.
---
+++ medium/max_number_of_vowels-1456.py
@@ -0,0 +1,61 @@
+"""
+Problem Title: Maximum Number of Vowels in a Substring of Given Length
+Problem ID: 1456
+Difficulty: Medium
+URL: https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description/
+
+Description:
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+
+Example 1:
+
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+Example 2:
+
+Input: s = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+Example 3:
+
+Input: s = "leetcode", k = 3
+Output: 2
+Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+1 <= k <= s.length
+"""
+
+vowels = {"a", "e", "i", "o", "u"}
+
+def max_vowels(s: str, k: int) -> int:
+    size = len(s)
+    result = 0
+    current_window = s[:k]
+    for letter in current_window:
+        if letter in vowels:
+            result += 1
+    i = 1
+    current = result
+    while i + k <= size:
+        if s[i - 1] in vowels:
+            current -= 1
+        if s[i + k - 1] in vowels:
+            current += 1
+        result = max(result, current)
+        i += 1
+    return result
+
+
+if __name__ == '__main__':
+    string_input = "weallloveyou"
+    window = 7
+    print(max_vowels(string_input, window))
